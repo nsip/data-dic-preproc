f0fb0af1ff04e3d2623c60a27e4d8e772dd8df69
Qing Miao <digisanQM@outlook.com>
2022-06-19 13:27:22 +1000

    * preproc

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"os"
 	"path/filepath"
-	"strings"
 
-	. "github.com/digisan/go-generics/v2"
+	// . "github.com/digisan/go-generics/v2"
 	fd "github.com/digisan/gotk/filedir"
 	gio "github.com/digisan/gotk/io"
 	lk "github.com/digisan/logkit"
-	"github.com/tidwall/gjson"
 )
 
 func init() {
@@ -24,102 +21,121 @@ func init() {
 //
 // 1) original => renamed --- cmd `go build -o rename`
 //
-func main() {
+// func main() {
 
-	var (
-		dirOriEntPtr = flag.String("oed", "./data/original", "original entities json data directory")
-		dirOriColPtr = flag.String("ocd", "./data/original/collections", "original collections json data directory")
-		dirRnEntPtr  = flag.String("red", "./data/renamed", "renamed entities json data directory")
-		dirRnColPtr  = flag.String("rcd", "./data/renamed/collections", "renamed collections json data directory")
-	)
+// 	var (
+// 		dirOriEntPtr = flag.String("oed", "./data/original", "original entities json data directory")
+// 		dirOriColPtr = flag.String("ocd", "./data/original/collections", "original collections json data directory")
+// 		dirRnEntPtr  = flag.String("red", "./data/renamed", "renamed entities json data directory")
+// 		dirRnColPtr  = flag.String("rcd", "./data/renamed/collections", "renamed collections json data directory")
+// 	)
 
-	flag.Parse()
+// 	flag.Parse()
 
-	//////////////////////////////////////////////////////////////
+// 	//////////////////////////////////////////////////////////////
 
-	dirOriEnt, dirRnEnt := *dirOriEntPtr, *dirRnEntPtr
+// 	dirOriEnt, dirRnEnt := *dirOriEntPtr, *dirRnEntPtr
 
-	gio.MustCreateDir(dirRnEnt)
+// 	gio.MustCreateDir(dirRnEnt)
 
-	// clear destination dir for putting renamed file
-	lk.FailOnErr("%v", fd.RmFilesIn(dirRnEnt, false, true, "json"))
+// 	// clear destination dir for putting renamed file
+// 	lk.FailOnErr("%v", fd.RmFilesIn(dirRnEnt, false, true, "json"))
 
-	// make sure each file's name is its entity value
-	FixFileName(dirOriEnt, dirRnEnt)
+// 	// make sure each file's name is its entity value
+// 	FixFileName(dirOriEnt, dirRnEnt)
 
-	//////////////////////////////////////////////////////////////
+// 	//////////////////////////////////////////////////////////////
 
-	dirOriCol, dirRnCol := *dirOriColPtr, *dirRnColPtr
+// 	dirOriCol, dirRnCol := *dirOriColPtr, *dirRnColPtr
 
-	gio.MustCreateDir(dirRnCol)
+// 	gio.MustCreateDir(dirRnCol)
 
-	// clear destination dir for putting renamed file
-	lk.FailOnErr("%v", fd.RmFilesIn(dirRnCol, false, true, "json"))
+// 	// clear destination dir for putting renamed file
+// 	lk.FailOnErr("%v", fd.RmFilesIn(dirRnCol, false, true, "json"))
 
-	// make sure each file's name is its entity value
-	FixFileName(dirOriCol, dirRnCol)
+// 	// make sure each file's name is its entity value
+// 	FixFileName(dirOriCol, dirRnCol)
 
-	// <------------------------------------------------------------------------------------->
+// 	// <------------------------------------------------------------------------------------->
 
-	mChk := map[string][]string{
-		dirRnEnt: {"Element", "Object", "Abstract Element"},
-		dirRnCol: {"Collection"},
-	}
+// 	mChk := map[string][]string{
+// 		dirRnEnt: {"Element", "Object", "Abstract Element"},
+// 		dirRnCol: {"Collection"},
+// 	}
 
-	for _, dir := range []string{dirRnEnt, dirRnCol} {
-		fs, err := os.ReadDir(dir)
-		lk.FailOnErr("%v", err)
-		for _, f := range fs {
-			if fname := f.Name(); strings.HasSuffix(fname, ".json") {
-				fpath := filepath.Join(dir, fname)
-				data, err := os.ReadFile(fpath)
-				lk.FailOnErr("%v", err)
-				lk.WarnOnErrWhen(NotIn(gjson.Get(string(data), "Metadata.Type").String(), mChk[dir]...), "%v@%s", errors.New("ERROR TYPE"), fpath)
-			}
-		}
-	}
+// 	for _, dir := range []string{dirRnEnt, dirRnCol} {
+// 		fs, err := os.ReadDir(dir)
+// 		lk.FailOnErr("%v", err)
+// 		for _, f := range fs {
+// 			if fname := f.Name(); strings.HasSuffix(fname, ".json") {
+// 				fpath := filepath.Join(dir, fname)
+// 				data, err := os.ReadFile(fpath)
+// 				lk.FailOnErr("%v", err)
+// 				lk.WarnOnErrWhen(NotIn(gjson.Get(string(data), "Metadata.Type").String(), mChk[dir]...), "%v@%s", errors.New("ERROR TYPE"), fpath)
+// 			}
+// 		}
+// 	}
 
-}
+// }
 
 //////////////////////////////////////////////////////////////////////////////////////
 
 //
 // 2) renamed => out / err --- cmd: `go build -o preproc`
 //
-// func main() {
+func main() {
 
-// 	dirInPtr := flag.String("in", "./data", "data directory")
-// 	flag.Parse()
+	var (
+		dirInEntPtr  = flag.String("ie", "./data/renamed", "input entities data directory")
+		dirInColPtr  = flag.String("ic", "./data/renamed/collections", "input collections data directory")
+		dirOutEntPtr = flag.String("oe", "./data/out", "output entities data directory")
+		dirOutColPtr = flag.String("oc", "./data/out/collections", "output collections data directory")
+		dirErrEntPtr = flag.String("ee", "./data/err", "error entities data directory")
+		dirErrColPtr = flag.String("ec", "./data/err/collections", "error collections data directory")
+	)
 
-// 	dirIn := *dirInPtr
+	flag.Parse()
 
-// 	out := filepath.Join(dirIn, "out")       // "out" is final output directory for ingestion
-// 	errfolder := filepath.Join(dirIn, "err") // "err" is for incorrect format json dump into
-// 	lk.FailOnErr("%v", os.RemoveAll(out))
-// 	lk.FailOnErr("%v", os.RemoveAll(errfolder))
+	mInOut := map[string]string{
+		*dirInEntPtr: *dirOutEntPtr,
+		*dirInColPtr: *dirOutColPtr,
+	}
+	mInErr := map[string]string{
+		*dirInEntPtr: *dirErrEntPtr,
+		*dirInColPtr: *dirErrColPtr,
+	}
 
-// 	Preproc(dirIn, out, errfolder)
+	for _, dir := range []string{*dirInEntPtr, *dirInColPtr} {
 
-// 	/////////////////////////////////////////////////////////////////////
+		out := mInOut[dir]       // "out" is final output directory for ingestion
+		errfolder := mInErr[dir] // "err" is for incorrect format json dump into
 
-// 	files, _, err := fd.WalkFileDir(out, false)
-// 	lk.FailOnErr("%v", err)
+		lk.FailOnErr("%v", os.RemoveAll(out))
+		lk.FailOnErr("%v", os.RemoveAll(errfolder))
 
-// 	linkCol := LinkEntities(files...)
+		Preproc(dir, out, errfolder)
 
-// 	js, err := Link2JSON(linkCol, "")
-// 	lk.FailOnErr("%v", err)
+		/////////////////////////////////////////////////////////////////////
 
-// 	lk.FailOnErr("%v", os.WriteFile(filepath.Join(out, "class-link.json"), []byte(js), os.ModePerm))
+		files, _, err := fd.WalkFileDir(out, false)
+		lk.FailOnErr("%v", err)
 
-// 	/////////////////////////////////////////////////////////////////////
+		linkCol := LinkEntities(files...)
 
-// 	osdir := filepath.Join(out, "path_val")
-// 	gio.MustCreateDir(osdir)
-// 	fpaths, _, err := fd.WalkFileDir(out, false)
-// 	lk.FailOnErr("%v", err)
-// 	for entity, js := range GenEntityPathVal(fpaths...) {
-// 		lk.FailOnErr("%v", os.WriteFile(filepath.Join(osdir, entity+".json"), []byte(js), os.ModePerm))
-// 	}
+		js, err := Link2JSON(linkCol, "")
+		lk.FailOnErr("%v", err)
 
-// }
+		lk.FailOnErr("%v", os.WriteFile(filepath.Join(out, "class-link.json"), []byte(js), os.ModePerm))
+
+		/////////////////////////////////////////////////////////////////////
+
+		osdir := filepath.Join(out, "path_val")
+		gio.MustCreateDir(osdir)
+		fpaths, _, err := fd.WalkFileDir(out, false)
+		lk.FailOnErr("%v", err)
+		for entity, js := range GenEntityPathVal(fpaths...) {
+			lk.FailOnErr("%v", os.WriteFile(filepath.Join(osdir, entity+".json"), []byte(js), os.ModePerm))
+		}
+
+	}
+}
